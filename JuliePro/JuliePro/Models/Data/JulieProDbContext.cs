43644a23fa47275e83487125909420949a2a05d3
Hugo de Bellefeuille <6295691@cegepmontpetit.ca>
2025-06-16 13:33:41 -0400

    Ajout Speciality
    Ajout de la classe Speciality et de son seed

---
+++ JuliePro/JuliePro/Models/Data/JulieProDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JuliePro.Models.Data
+{
+    public class JulieProDbContext : DbContext
+    {
+        public JulieProDbContext(DbContextOptions<JulieProDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Speciality>().HasData(new Speciality() { Id = 1, Name = "Perte de poids" });
+            builder.Entity<Speciality>().HasData(new Speciality() { Id = 2, Name = "Course" });
+            builder.Entity<Speciality>().HasData(new Speciality() { Id = 3, Name = "Halthérophilie" });
+            builder.Entity<Speciality>().HasData(new Speciality() { Id = 4, Name = "Réhabilitation" });
+        }
+
+        public DbSet<Speciality> Specialities { get; set; }
+    }
+}
--- JuliePro/JuliePro/Models/Speciality.cs
+++ JuliePro/JuliePro/Models/Speciality.cs
@@ -4,6 +4,10 @@ namespace JuliePro.Models
 {
     public class Speciality
     {
+        [Key]
+        public int Id { get; set; }
+
+
         [StringLength(20, MinimumLength = 5)]
         public string Name { get; set; }
     }
